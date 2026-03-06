9ff33a8f735fc6dbc836388f5946719e94ec6a56
khalaffarhan <khalaffarhan54@gmail.com>
2026-03-06 16:28:02 +0700

    commit 4

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -47,3 +47,28 @@ void display()
     }
 }
 
+void bubblesort()
+{
+    cout << "\n=================" << endl;
+    cout << "Hasil Setiap Pass" << endl;
+    cout << "\n===============" << endl;
+    for (int pass = 1; pass < n; pass++)
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+        cout << "Pass ke-" << pass << ":\t";
+        for (int k = 0; k < n; k++)
+        {
+            cout << a[k] << " ";
+        }
+        cout << endl;
+    }
+}
+
