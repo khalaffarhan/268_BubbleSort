e6cf4125947860da1db64017dbcaebe364ee6b13
khalaffarhan <khalaffarhan54@gmail.com>
2026-03-06 16:23:03 +0700

    commit 1

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,32 +4,3 @@ using namespace std;
 int a[20];
 int n;
 
-void input()
-{
-    while (true)
-    {
-        cout << "Enter the number of elements in the array: ";
-        cin >> n;
-
-        if (n <= 20)
-        {
-            break;
-        }
-        else
-        {
-            cout << "Error: Maksimal input array adalah 20\n";
-        }
-    }
-
-    cout << endl;
-    cout << "=====================" << endl;
-    cout << "Masukkan elemen Array" << endl;
-    cout << "=====================" << endl;
-
-    for (int i = 0; i < n; i++) // i++ / i = i + 1
-    {
-        cout << "Masukkan Nilai ke-" << (i + 1) << ": ";
-        cin >> a[i];
-    }
-}
-
